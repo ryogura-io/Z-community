7a26f0308a15743d2866f9471efe90408f0c25c1
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-09-20 04:33:17 +0100

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema({
-  groupId: String,
+  groupId: { type: String, required: true },
+  groupName: { type: String, default: "" },
   activeSpawn: {
     cardId: { type: mongoose.Schema.Types.ObjectId, ref: "Card" }, // from Cards DB
     captcha: String
@@ -9,5 +10,15 @@ const groupSchema = new mongoose.Schema({
   status: { type: String, enum: ["enabled", "disabled"], default: "disabled" },
   spawn: { type: String, enum: ["enabled", "disabled"], default: "enabled" }
 });
+
+groupSchema.methods.updateName = async function (sock) {
+  try {
+    const metadata = await sock.groupMetadata(this.groupId);
+    this.groupName = metadata.subject;
+    await this.save();
+  } catch (err) {
+    console.error("Failed to update group name:", err);
+  }
+};
 
 module.exports = mongoose.model("Group", groupSchema);
