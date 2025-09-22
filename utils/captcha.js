8ca60ca96b56359a96878413d02b52197f0e083b
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-09-22 22:57:24 +0100

    Delete utils directory

--- utils/captcha.js
+++
@@ -1,6 +0,0 @@
-function generateCaptcha(length = 5) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
-}
-
-module.exports = generateCaptcha;
