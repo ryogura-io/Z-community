4037c6d0c96ea7e06bf96c44a80117dac9d64ae0
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-09-29 02:04:21 +0100

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -3,14 +3,27 @@ const mongoose = require("mongoose");
 const groupSchema = new mongoose.Schema({
   groupId: { type: String, required: true },
   groupName: { type: String, default: "" },
+
   activeSpawn: {
     cardId: { type: mongoose.Schema.Types.ObjectId, ref: "Card" }, // from Cards DB
-    captcha: String
+    captcha: String,
   },
+
+  // Track last shown character in group
+  lastCharacter: {
+    id: { type: String, default: "" },
+    slug: { type: String, default: "" },
+    name: { type: String, default: "" },
+    romaji_name: { type: String, default: "" },
+    display_picture: { type: String, default: "" },
+    description: { type: String, default: "" },
+    appearances: [{ name: { type: String } }], // array of appearances
+    url: { type: String, default: "" },
+  },
+
   status: { type: String, enum: ["enabled", "disabled"], default: "disabled" },
   spawn: { type: String, enum: ["enabled", "disabled"], default: "enabled" },
-  slot: { type: String, enum: ["enabled", "disabled"], default: "disabled" }
+  slot: { type: String, enum: ["enabled", "disabled"], default: "disabled" },
 });
 
 module.exports = mongoose.model("Group", groupSchema);
-
