10c80c6be3229d6c593ccf118e394d92fc67064c
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-10-04 16:01:00 +0100

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,34 +1,36 @@
-const helpCommand = require('./help');
-const adminCommands = require('./admin');
-const groupCommands = require('./group');
-const mediaCommands = require('./media');
-const utilityCommands = require('./utility');
-const gameCommands = require('./games');
-const downloaderCommands = require('./downloaders');
+const helpCommand = require("./help");
+const adminCommands = require("./admin");
+const groupCommands = require("./group");
+const mediaCommands = require("./media");
+const utilityCommands = require("./utility");
+const gameCommands = require("./games");
+const downloaderCommands = require("./downloaders");
 const cards = require("./cards");
-const card2Commands = require('./card2');
-const economyCommands = require('./economy');
-const familiaCommands = require('./familia');
-const coreCommands = require('./core');
-const moderatorCommands = require('./moderator');
-const reactionCommands = require('./reactions');
+const card2Commands = require("./card2");
+const economyCommands = require("./economy");
+const familiaCommands = require("./familia");
+const coreCommands = require("./core");
+const moderatorCommands = require("./moderator");
+const reactionCommands = require("./reactions");
+const eventCommands = require("./event");
 
 // Combine all commands
 const rawCommands = {
-    ...helpCommand,
-    ...adminCommands,
-    ...groupCommands,
-    ...mediaCommands,
-    ...utilityCommands,
-    ...gameCommands,
-    ...downloaderCommands,
-    ...cards,
-    ...card2Commands,
-    ...economyCommands,
-    ...familiaCommands,
-    ...coreCommands,
-    ...moderatorCommands,
-    ...reactionCommands,
+  ...helpCommand,
+  ...adminCommands,
+  ...groupCommands,
+  ...mediaCommands,
+  ...utilityCommands,
+  ...gameCommands,
+  ...downloaderCommands,
+  ...cards,
+  ...card2Commands,
+  ...economyCommands,
+  ...familiaCommands,
+  ...coreCommands,
+  ...moderatorCommands,
+  ...reactionCommands,
+  ...eventCommands,
 };
 
 const commands = {};
