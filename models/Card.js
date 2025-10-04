6fc9783f65ae634153cc6e5ecba59719872aea3a
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-10-04 18:47:00 +0100

    Update Card.js

--- models/Card.js
+++ models/Card.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
-const cardSchema = new mongoose.Schema({
+const ecardSchema = new mongoose.Schema({
   name: String,
   tier: String, 
   series: String,
   img: String,
   maker: String,
-  url: String
+  url: String, 
+  event: { type: String, default: null },
+  isEvent: { type: Boolean, default: false },
 });
 
-module.exports = mongoose.model("Card", cardSchema, "cards");
+module.exports = mongoose.model("eCard", ecardSchema, "eventCards");
